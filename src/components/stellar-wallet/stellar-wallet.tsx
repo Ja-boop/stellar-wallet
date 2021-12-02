0b69a80fa0a051f8a109a4cebf8b840fa7d63bc7
Ja-boop <agusmisiones@gmail.com>
2021-12-02 20:32:34 -0300

    Initial commit

---
+++ src/components/stellar-wallet/stellar-wallet.tsx
@@ -0,0 +1,23 @@
+import { Component, h } from '@stencil/core';
+import StellarSdk from "stellar-sdk";
+
+@Component({
+  tag: 'stellar-wallet',
+  styleUrl: 'stellar-wallet.scss',
+  shadow: true,
+})
+export class StellarWallet {
+
+  render() {
+    
+
+    return (
+      <h1>
+        {!!StellarSdk
+          ? "The StellarSdk is ready to rock 🤘"
+          : "Uh oh, the StellarSdk is missing 😱"}
+      </h1>
+    );
+  }
+
+}
